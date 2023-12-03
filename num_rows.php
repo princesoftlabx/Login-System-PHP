2734c1c10a26cebaf38fac1255498c9b748a5807
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2023-12-03 15:02:46 +0530

    Update num_rows.php

--- num_rows.php
+++ num_rows.php
@@ -1,9 +1,9 @@
 <?php
-$con=mysqli_connect("localhost","root","","validation");
+include("partials/dbconnect.php");
 // Check connection
 if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "Failed to connect to MySQL: ". mysqli_connect_error();
   }
 
 $sql="SELECT lname,email FROM formdata ORDER BY lname";
